4fad823fc2f3b28406d35290893676c9335e735e
Viacheslav Ermakov <slavaskainet@gmail.com>
2020-06-16 20:05:37 +0300

    remove loaders

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: ['./src/index.tsx'],
+    index: ["./src/index.tsx"],
   },
 
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[name].[hash].bundle.js',
-    chunkFilename: '[id].js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].[hash].bundle.js",
+    chunkFilename: "[id].js",
+    publicPath: "/",
   },
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss', '.css', '.svg'],
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".scss", ".css", ".svg"],
     alias: {
-      src: path.resolve(__dirname, '../src'),
-      components: path.resolve(__dirname, '../src/components'),
-      model: path.resolve(__dirname, '../src/model'),
+      src: path.resolve(__dirname, "../src"),
+      components: path.resolve(__dirname, "../src/components"),
+      model: path.resolve(__dirname, "../src/model"),
     },
   },
 
@@ -28,38 +28,14 @@ module.exports = {
       {
         test: /\.(ts|tsx)$/,
         exclude: /(node_modules)/,
-        use: ['ts-loader'],
+        use: ["ts-loader"],
       },
       {
         test: /\.(html)$/,
         use: {
-          loader: 'html-loader',
+          loader: "html-loader",
           options: {
-            attrs: [':data-src'],
             minimize: true,
-          },
-        },
-      },
-      {
-        test: /\.(png|jpg|gif|jpeg|ttf|svg|mp4)$/,
-        type: 'javascript/auto',
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-              esModule: false,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.json$/,
-        loader: 'file-loader',
-        type: 'javascript/auto',
-        options: {
-          name() {
-            return '[path][name].[ext]';
           },
         },
       },
@@ -68,10 +44,10 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'chulakov test app',
-      template: './public/index.html',
-      filename: 'index.html',
-      jsExtension: '.gz',
+      title: "chulakov test app",
+      template: "./public/index.html",
+      filename: "index.html",
+      jsExtension: ".gz",
     }),
   ],
 };
